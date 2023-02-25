5c498f0e984142e8faca9b28a0b1c5df931889dc
Maya Man <mayaman26@gmail.com>
2023-02-25 02:17:21 -0800

    added audio description detection in single caption, edited type sizes, updated selection color, etc

--- script.js
+++ script.js
@@ -14,39 +14,28 @@ let height = window.innerHeight;
 let typeSizeMin = 12;
 let typeSizeMax = 88;
 let speedMode = false;
-
-let testCaptionIndex = 0;
-// No more than 7 words?
-
+const originalCaptions = [...captions];
+const originalComments = [...comments];
 let settingOptions = ["commentOnly", "captionOnly"];
-currentSetting = "captionOnly";
-captionDiv.style.display = "inline";
-document.getElementById("comments").style.display = "none";
 let switchMode = true;
 let commentChoiceIndex = 0;
 let captionChoiceIndex = 0;
 let started = false;
 let typingTimeout;
+let captionTypingTimeout;
 let newCommentTimeout;
 let captionTimeout;
-
-let colors = ["#4a2613", "#693517", "#93532e", "#ba6a36", "#c37f46", "#db9765", "#ecae78", "#eebf9d", "#FEC7CF", "#FCA88F", "#C25F6D", "#D9D9D9", "#000000"];
-// let colorpalette = ["#C2B19F", "#D3DBD4", "#865140", "#ECAC9D", "#DC8BA8", "#A88373", "#A69A92", "#89919B", "#404143"];
-// let colorpalette = ["#DC8BA8", "#A88373", "#89919B", "#A69A92", "#865140"];
-// let before saturaiton colorpalette = ["#DC8BA8", "#ECAC9D", "#89919B","#A88373", "#865140"];
 let colorpalette = ["#f38db2", "#8799ac", "#874830", "#c28c70", "#ecac9d"];
-// "#A69A92"
-
 let currentBackgroundColor = colorpalette[Math.floor(Math.random() * colorpalette.length)];
+
+currentSetting = "captionOnly";
+captionDiv.style.display = "inline";
+document.getElementById("comments").style.display = "none";
 /* 💋 TODO
   + !!!!! Now
-  - [] Change top text to no longer type out but fully switch like captions
-  - [] Add way more comments
-  - [] Choose title for piece
-  - [] Create generative system for caption text
-  - [] Fix issue with set timeout freaking out
   - [] Remove comment/caption once it's written, then restart once have shown all of them
   + DONE
+  - [x] if it's a [] type it out all at once
   - [x] Separate caption and comment modes, don't need to be intertwined
   - [x] Find warmer color for background
   - [x] Key pressed to toggle between views
@@ -55,6 +44,8 @@ let currentBackgroundColor = colorpalette[Math.floor(Math.random() * colorpalett
 */
 
 console.log("★.｡.:*☆:**:. ⓦ𝕖𝓑s𝕚𝓉𝐄 Ｂʸ 𝓶ⓐ𝐲ᗩ 𝐌𝕒𝓝 .:**:.☆*.:｡.★ ♡ www.mayaontheinter.net ♡ 萬美亞");
+console.log("https://www.youtube.com/watch?v=dntyqXZLk3g&list=PLztAHXmlMZFS9ZN7GTlZ2UOB2JmxICdt8&index=7&ab_channel=Vogue");
+console.log(comments[Math.floor(Math.random() * comments.length)]);
 window.addEventListener("resize", onWindowResize);
 
 setTypeSize();
@@ -68,8 +59,7 @@ function toggleModes(newMode) {
         chooseNewComment();
         renderText();
         clearTimeout(captionTimeout);
-        // document.body.style.backgroundColor = "#f0f5f7";
-        // document.body.style.backgroundColor = currentBackgroundColor;
+        clearTimeout(captionTypingTimeout);
     } else {
         captionDiv.style.display = "inline";
         document.getElementById("comments").style.display = "none";
@@ -77,21 +67,15 @@ function toggleModes(newMode) {
         renderText();
         clearTimeout(newCommentTimeout);
         clearTimeout(typingTimeout);
-        // document.body.style.backgroundColor = currentBackgroundColor;
-
     }
 }
 
-// Kick off rendering of type out of that thing
-// setTimeout(renderText, 47);
-renderText();
-
 function setTypeSize() {
-    console.log('setting type size: ', width);
+    // console.log('setting type size: ', width);
     if (width >= 4000) {
         // 4K
-        typeSizeMin = 64;
-        typeSizeMax = 248;
+        typeSizeMin = 56;
+        typeSizeMax = 108;
     } else if (width > 2000) {
         // TV SIZE
         typeSizeMin = 24;
@@ -120,8 +104,8 @@ function renderComment() {
         mark.innerHTML = newString.substring(0, currentStringIndex);
         currentStringIndex++;
         let newInterval = 47 + Math.random() * 111;
+
         // Testing
-        // newInterval = 11;
         if (speedMode) {
             newInterval = 1;
         }
@@ -132,17 +116,10 @@ function renderComment() {
         currentStringIndex = 0;
         chooseNewComment();
         let newInterval = 2222 + Math.random() * 5555;
-        // newInterval = 11;
         if (speedMode) {
             newInterval = 1;
         }
         newCommentTimeout = setTimeout(() => {
-            // let newBackgroundColor = colorpalette[Math.floor(Math.random() * colorpalette.length)];
-            // while (currentBackgroundColor == newBackgroundColor) {
-            //     newBackgroundColor = colorpalette[Math.floor(Math.random() * colorpalette.length)];
-            // }
-            // document.body.style.backgroundColor = newBackgroundColor;
-            // currentBackgroundColor = newBackgroundColor;
             renderText();
         }, newInterval);
     }
@@ -154,29 +131,17 @@ function renderCaption() {
             currentCaption = currentCaption + " " + newCaptionArray[currentCaptionIndex];
         } else {
             currentCaption = newCaptionArray[currentCaptionIndex];
-
         }
         captionDiv.innerText = currentCaption;
-        // captionDiv.innerText = "It's just to give me a little bit of depth.";
         currentCaptionIndex++;
         const newInterval = 111 + Math.random() * 333;
-        // Testing
-        // newInterval = 11;
-        typingTimeout = setTimeout(() => {
+        captionTypingTimeout = setTimeout(() => {
             renderText();
         }, newInterval);
     } else {
         chooseNewCaption();
-        // Set for new caption to appear
         const newInterval = 3333 + Math.random() * 11111;
         captionTimeout = setTimeout(() => {
-            let newBackgroundColor = colorpalette[Math.floor(Math.random() * colorpalette.length)];
-            while (currentBackgroundColor == newBackgroundColor) {
-                newBackgroundColor = colorpalette[Math.floor(Math.random() * colorpalette.length)];
-            }
-            // document.body.style.backgroundColor = newBackgroundColor;
-            // document.body.style.backgroundColor = "#f0f5f7";
-            currentBackgroundColor = newBackgroundColor;
             renderCaption();
         }, newInterval);
     }
@@ -186,34 +151,65 @@ function renderCaption() {
 function chooseNewCaption() {
     currentCaptionIndex = 0;
     currentCaption = "";
-    // newCaption = captions[Math.floor(Math.random() * captions.length)].replaceAll('.', '').replaceAll(',', '');
-    newCaption = captions[testCaptionIndex].replaceAll('.', '').replaceAll(',', '');
-    testCaptionIndex++;
-    // newCaption = "It may be a bit controversial, but I like it and I was able to find it, so I'm gonna";
-    console.log('cap lenght: ', newCaption.length);
+    const newCaptionIndex = Math.floor(Math.random() * captions.length);
+    
+    newCaption = captions[newCaptionIndex].replaceAll('.', '').replaceAll(',', '');
+    captions.splice(newCaptionIndex, 1);
+    
+    // If all captions have been shown, reset caption library
+    if (captions.length < 1) {
+        captions = [...originalCaptions];
+    }
+
     const maxCaptionLength = 86;
     while (newCaption.length > maxCaptionLength) {
         newCaption = captions[Math.floor(Math.random() * captions.length)].replaceAll('.', '').replaceAll(',', '');
     }
-    // newCaption = newCaption[0].toLowerCase() + newCaption.substring(1, newCaption.length);
+
     newCaption = newCaption.toLowerCase();
     newCaption = newCaption.replaceAll(/\bi\b/g, "I"); // Only an isolated I should be uppercase
     newCaption = newCaption.replaceAll(/i'/g, "I'");
 
-    const doubleLineLength = 42;
+    const doubleLineLength = 43;
 
     if (isAudioDescription(newCaption)) {
         // [Audio description]
         newCaptionArray = [newCaption];
     } else {
         newCaptionArray = newCaption.split(" ");
-
+        // console.log(audioPop(newCaptionArray));
+        newCaptionArray = audioPop(newCaptionArray);
         if (newCaption.length >= doubleLineLength) {
             const halfCaptionIndex = Math.floor(newCaptionArray.length * .47);
             newCaptionArray[halfCaptionIndex] = newCaptionArray[halfCaptionIndex] + "\n";
-            // newCaption = newCaption.substring(0, halfCaption) + "\n" + newCaption.substring(halfCaption, newCaption.length);
         }
     }
+}
+
+// TO DO
+function audioPop(array) {
+    let newArray = [];
+    // let descriptionIndex = -1;
+    let description = "";
+    let describing = false;
+
+    for (let i = 0; i < array.length; i++) {
+        let currentWord = array[i];
+        if (currentWord.indexOf("[") > -1) {
+            description += currentWord;
+            describing = true;
+        } else if (describing) {
+            description = description + " " + currentWord;
+            if (currentWord.indexOf("]") > -1) {
+                describing = false;
+                newArray.push(description);
+                description = "";
+            }
+        } else {
+            newArray.push(currentWord);
+        }
+    }
+    return newArray;
 }
 
 function isAudioDescription(caption) {
@@ -229,8 +225,16 @@ function isAudioDescription(caption) {
 
 // Choose random new comment
 function chooseNewComment() {
-    newString = comments[Math.floor(Math.random() * comments.length)];
+
+    const newCommentIndex = Math.floor(Math.random() * comments.length);
+    newString = comments[newCommentIndex];
     const newFontSize = typeSizeMin + Math.random() * typeSizeMax;
+
+    comments.splice(newCommentIndex, 1);
+    // If all comments have been shown, reset comment library
+    if (comments.length < 1) {
+        comments = originalComments;
+    }
 
     div = document.createElement("div");
     div.classList.add("text");
@@ -258,40 +262,19 @@ function chooseNewComment() {
 
     const maxHeight = testDiv.clientHeight;
 
-    // const maxWidth = width - testDiv.clientWidth - widthPadding;
-    // const maxWidth = width / 4 + Math.random() * width * 0.75;
-
     const divTop = randomIntFromInterval(0, height - maxHeight);
-    // console.log('div top: ', divTop)
     div.style.top = divTop + "px";
-    // console.log('max width: ', maxWidth);
-
     div.style.left = randomIntFromInterval(0, width - maxWidth) + "px";
     div.style.maxWidth = maxWidth + "px";
     div.style.fontSize = newFontSize + "px";
     div.style.opacity = "1";
-
-    // div.style.textShadow = "0 0 " + (newFontSize / 22 ) + "px #ffffff, " + "0 0 " + (newFontSize / 16) + "px #ffffff";
-    // div.style.textShadow = "0 0 " + (newFontSize / 22 ) + "px #000000, " + "0 0 " + (newFontSize / 16) + "px #000000";
-
-
-    // div.style.textShadow = "0 0 " + (newFontSize / 33) + "px #000000";
-
     mark = document.createElement("mark");
     mark.style.color = colorpalette[Math.floor(Math.random() * colorpalette.length)];
-    // mark.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
     div.appendChild(mark);
     document.getElementById("comments").appendChild(div);
     testDiv.remove();
     allText.push(div);
-    // freaks out around 280
-
-
-    if (allText.length > 3) {
-        // console.log('removing a div');
-        // let toRemove = allText.shift();
-        // toRemove.remove();
-
+    if (allText.length > 47) {
         for (let i = 0; i < allText.length / 2; i++) {
             let currentTextDiv = allText[i];
             let currentOpacity = currentTextDiv.style.opacity;
